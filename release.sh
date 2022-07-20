6757ff5af215a176da2c1a7fce1c7b61168c26b0
Alex Christian <alexchristianqr@gmail.com>
2022-07-20 01:28:35 -0500

    [added] release Shell script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is the automated release script
+
+# guard against stupid
+if [ -z "$1" ]; then
+   echo "You must specify a new version level: [patch, minor, major]";
+   exit 1;
+fi
+
+tagVersion=$1
+
+set -e
+
+#git tag -d "v$tagVersion"
+git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
+git push origin "v$tagVersion"
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m "new deployment for release v$tagVersion"
+git push -f git@github.com:alexchristianqr/app-todolist.git main:gh-pages
+
+cd -
