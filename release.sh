e317744cddf48c5764aeb372b5415def64952382
Alex Christian <alexchristianqr@gmail.com>
2022-07-21 03:04:21 -0500

    [updated] release

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# This is the automated release script
 
-# guard against stupid
 if [ -z "$1" ]; then
    echo "You must specify a new version level: [patch, minor, major]";
    exit 1;
@@ -11,7 +9,6 @@ tagVersion=$1
 
 set -e
 
-#git tag -d "v$tagVersion"
 git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
 git push origin "v$tagVersion"
 
