86da3697ffe67906c5971ea17d87f591e8d49c1e
Alex Christian <alexchristianqr@gmail.com>
2022-07-23 15:19:16 -0500

    [updated] html

--- src/components/common/MyFooter.vue
+++ src/components/common/MyFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <b-form-group class="pt-lg-0 pt-3">
-    <div class="  ">
+    <div>
       <span class="mr-1">{{ $t('TodoList.footer.a') }}</span>
       <img src="@/assets/logo.png" :width="18" class="mr-1" alt="Vuejs" title="Vuejs" />
-      <span
-        >{{ $t('TodoList.footer.b') }} <b-link target="_blank" href="https://github.com/alexchristianqr">@alexchristianqr</b-link> {{ $t('TodoList.footer.c') }}
-        <span class="text-danger" style="font-size: 18px">&hearts;</span></span
-      >
+      <span>{{ $t('TodoList.footer.b') }} </span>
+      <b-link target="_blank" href="https://github.com/alexchristianqr">@alexchristianqr </b-link>
+      <span>{{ $t('TodoList.footer.c') }} </span>
+      <span class="text-danger" style="font-size: 18px">&hearts;</span>
     </div>
   </b-form-group>
 </template>
