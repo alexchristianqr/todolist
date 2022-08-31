9de5964773a648d931c7d5fa65a17c2a491e495b
Alex Christian <alexchristianqr@gmail.com>
2022-08-31 03:09:05 -0500

    [updated] refactor

--- src/store/modules/tasks/state.js
+++ src/store/modules/tasks/state.js
@@ -1,17 +1,16 @@
 const state = {
   paginate: {
-    // data: [
-    //   {
-    //     id: 1,
-    //     title: 'Tarea #1',
-    //     description: 'Ejemplo descriptivo de mi tarea #1',
-    //     expiredAt: '2099-01-01 23:59:00',
-    //     updatedAt: '2022-01-01 18:00:00',
-    //     createdAt: '2022-01-01 18:00:00',
-    //     status: false,
-    //   },
-    // ],
-    data:[{"id":12,"title":"dsfsdgfdg","description":"fdgdfgfdg","expiredAt":"[native Date Wed Aug 31 2022 01:18:26 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:18:26 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:18:26 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":11,"title":"dsfsdfsd","description":"fsdfsdfsdf","expiredAt":"[native Date Wed Aug 31 2022 01:18:10 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:18:10 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:18:10 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":10,"title":"sdfsdfds","description":"fsdfsdfsd","expiredAt":"[native Date Wed Aug 31 2022 01:18:07 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:18:07 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:18:07 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":9,"title":"dsfsdfs","description":"dfsdfsdfsdf","expiredAt":"[native Date Wed Aug 31 2022 01:18:03 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:18:03 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:18:03 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":8,"title":"dsfsdfsdf","description":"dsfsdfsdf","expiredAt":"[native Date Wed Aug 31 2022 01:17:56 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:56 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:56 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":7,"title":"dsfdsfdsf","description":"sdfdsfsdfds","expiredAt":"[native Date Wed Aug 31 2022 01:17:53 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:53 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:53 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":6,"title":"sdfdsfdsf","description":"sdfdsfdsf","expiredAt":"[native Date Wed Aug 31 2022 01:17:50 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:50 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:50 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":5,"title":"sdfsdfsdf","description":"dsfdsfdsf","expiredAt":"[native Date Wed Aug 31 2022 01:17:44 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:44 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:44 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":4,"title":"dsfdsfsdf","description":"dsfsdfdsf","expiredAt":"[native Date Wed Aug 31 2022 01:17:41 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:41 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:41 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":3,"title":"dsfsdf","description":"sdfdsfsdf","expiredAt":"[native Date Wed Aug 31 2022 01:17:38 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:38 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:38 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":2,"title":"fsdfsdf","description":"fsd","expiredAt":"[native Date Wed Aug 31 2022 01:17:31 GMT-0500 (Peru Standard Time)]","updatedAt":"[native Date Wed Aug 31 2022 01:17:31 GMT-0500 (Peru Standard Time)]","createdAt":"[native Date Wed Aug 31 2022 01:17:31 GMT-0500 (Peru Standard Time)]","status":false,"isPost":true},{"id":1,"title":"Tarea #1","description":"Ejemplo descriptivo de mi tarea #1","expiredAt":"2099-01-01 23:59:00","updatedAt":"[native Date Wed Aug 31 2022 01:17:22 GMT-0500 (Peru Standard Time)]","createdAt":"2022-01-01 18:00:00","status":false}],
+    data: [
+      {
+        id: 1,
+        title: 'Tarea #1',
+        description: 'Ejemplo descriptivo de mi tarea #1',
+        expiredAt: '2099-01-01 23:59:00',
+        updatedAt: '2022-01-01 18:00:00',
+        createdAt: '2022-01-01 18:00:00',
+        status: false,
+      },
+    ],
     from: null,
     perPage: null,
     to: null,
